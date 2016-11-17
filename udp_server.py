c767984c8034e18fe84818e18dd07e0df0d28680
iamfuyu1031 <fu2@g.clemson.edu>
2016-11-17 14:38:32 -0500

    Update udp_server.py

--- udp_server.py
+++ udp_server.py
@@ -1,27 +1,7 @@
 import socket
 import sys
-'''
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Bind the socket to the port
-server_address = ('localhost', 50000)
-#print >>sys.stderr, 'starting up on %s port %s' % server_address
-sock.bind(server_address)
-
-while True:
-    #print >>sys.stderr, '\nwaiting to receive message'
-    data, address = sock.recvfrom(4096)
-    print data
-    print address
-    
-    #print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
-    #print >>sys.stderr, data
-    
-    if data:
-        sent = sock.sendto(data, address)
-        #print >>sys.stderr, 'sent %s bytes back to %s' % (sent, address)
-'''
+# A simple udp echo server
 BUFSIZE = 1024
 port = 50000
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
